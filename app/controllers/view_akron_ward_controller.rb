9c1ed3a3d9be70e5e1c23fafbe6eb9ca3a48e680
Rick Bacci <rickbacci@gmail.com>
2016-09-10 17:35:21 -0400

    add find_where action

--- app/controllers/view_akron_ward_controller.rb
+++ app/controllers/view_akron_ward_controller.rb
@@ -7,7 +7,51 @@ class ViewAkronWardController < ActionController::API
     render json: ViewAkronWard.where(find_params)
   end
 
-private
+  def find_where
+    column   = params[:column]
+    operator = params[:operator] || ''
+    value    = params[:value]
+
+    case operator
+    when 'less_than'
+      query_string = column << ' < ?'
+    when 'less_than_or_equal_to'
+      query_string = column << ' <= ?'
+    when 'greater_than'
+      query_string = column << ' > ?'
+    when 'greater_than_or_equal_to'
+      query_string = column << ' >= ?'
+    end
+
+    if find_where_columns_present?
+      if column_type(column) == :integer
+        render json: ViewAkronWard.where(query_string, value.to_i)
+      else
+        render json: ViewAkronWard.where(query_string, value)
+      end
+    else
+      render html: "Improper Query!"
+    end
+  end
+
+  private
+
+  def column_type(column)
+
+    column_type = ''
+
+    ViewAkronWard.columns.each do |obj|
+      if obj.name == column
+        column_type = obj.sql_type_metadata.type
+      end
+    end
+
+    column_type
+  end
+
+  def find_where_columns_present?
+    params[:column] && params[:operator] && params[:value]
+  end
 
   def find_params
     params.permit(:wardnumber,
@@ -27,6 +71,7 @@ private
                   :councilpersonweblink,
                   :wardglobalid,
                   :wardshape_starea,
-                  :wardshape_stlength)
+                  :wardshape_stlength,
+                  :operator)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :view_akron_ward, only: [:index]
 
   get "/view_akron_ward/find_by", to: "view_akron_ward#find_by"
+  get "/view_akron_ward/find_where", to: "view_akron_ward#find_where"
 
   root 'home#home'
 end
