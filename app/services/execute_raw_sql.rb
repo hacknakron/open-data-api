2e7abc2b555b9f865624d500b2018e890f7872c4
Marko Iskander <markoiskander@gmail.com>
2017-03-26 16:40:23 -0400

    Remove more unneeded files

--- app/services/execute_raw_sql.rb
+++
@@ -1,8 +0,0 @@
-class ExecuteRawSql
-  include ActiveRecord::ConnectionAdapters::Quoting
-
-  def query(_action, csv_file_name)
-    binding.pry
-    results = ActiveRecord::Base.connection.exec_query "SELECT data_controller('#{_action}', '#{csv_file_name}')"
-  end
-end
