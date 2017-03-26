e4a3d7f0e8db7e1c374d938af20e2d68144d6f4c
Byron Delpinal <byronddelpinal@gmail.com>
2017-03-26 18:14:49 -0400

    Change migration name to make rails happy

--- db/migrate/20170326220417_add_luc_codes.rb
+++ db/migrate/20170326220417_add_luc_codes.rb
@@ -1,4 +1,4 @@
-class AddLandUseCodes < ActiveRecord::Migration[5.0]
+class AddLucCodes < ActiveRecord::Migration[5.0]
   def change
     create_table :land_use_codes do |t|
       t.integer :object_id, unsigned: true
