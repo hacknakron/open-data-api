c771fc7a7e890dcf488f5a02bfa9e3815ab0fb8d
kharsch <kharsch@brandeis.edu>
2017-04-22 15:27:45 -0400

    add validation to models, add migration to land use code to change column name from value to use code

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -4,7 +4,11 @@ class Parcel < ApplicationRecord
 
   validates :object_id, presence: true
   validates :parcel_id, presence: true
-
+  validates :tax_year,  numericality: { only_integer: true }, presence: true
+  validates :use_class, presence: true
+  validates :use_code,  numericality: { only_integer: true }, presence: true
+ 
+  
   belongs_to :land_use_code
 
   has_many :parcel_sales
