795659480711dbb3fd2081f80e4995496db503cd
Nick Kinney <nkinney@virtualhold.com>
2017-04-22 14:03:29 -0400

    Changed the Sales/id route to select a singe sale based on the object_id.  Created parcels/id/sales to return a list of sales for a parcel.

--- app/controllers/api/sales_controller.rb
+++ app/controllers/api/sales_controller.rb
@@ -1,9 +1,13 @@
 class API::SalesController < API::BaseController
   def index
-    render json: ParcelSale.order(:parcel_id, :sales_date).page(params[:page]).per(params[:per_page])
+  	if(params[:parcel_id])
+  		render json: ParcelSale.where(parcel_id: params[:parcel_id]).order(:sales_date)
+    else
+    	render json: ParcelSale.order(:parcel_id, :sales_date).page(params[:page]).per(params[:per_page])
+    end
   end
 
   def show
-    render json: ParcelSale.where(parcel_id: params[:id]).order(:sales_date)
+    render json: ParcelSale.find_by(object_id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace 'api' do
     root 'root#index'
-    resources :parcels, only: [:index, :show]
+    resources :parcels, only: [:index, :show] do
+    	resources :sales, only: [:index]
+    end
     resources :sales, only: [:index, :show]
   end
 
