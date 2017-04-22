da8a2ed36f2a270e2ad51973a252b67dffbf0b97
Jonathan Knapp <jon@coffeeandcode.com>
2017-04-22 13:19:31 -0400

    Add explicit filtering params for Parcels API

--- app/controllers/api/parcels_controller.rb
+++ app/controllers/api/parcels_controller.rb
@@ -1,9 +1,20 @@
 class API::ParcelsController < API::BaseController
   def index
-    render json: Parcel.order(:id).page(params[:page]).per(params[:per_page])
+    render json: Parcel.order(:id).where(filter_params).page(params[:page]).per(params[:per_page])
   end
 
   def show
     render json: Parcel.find_by(parcel_id: params[:id])
   end
+
+  private
+
+    def filter_params
+      params.fetch('filters', {}).permit(
+        'object_id' => [],
+        'tax_year' => [],
+        'use_class' => [],
+        'use_code' => [],
+      )
+    end
 end
