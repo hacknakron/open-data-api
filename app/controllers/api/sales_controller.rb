14ebb33a42785fb58c9fc06f215011c3cced27e6
Nick Kinney <nkinney@virtualhold.com>
2017-04-22 13:15:47 -0400

    Renamed ParcelSales to Sales.

--- app/controllers/api/sales_controller.rb
+++ app/controllers/api/sales_controller.rb
@@ -1,4 +1,4 @@
-class API::ParcelSalesController < API::BaseController
+class API::SalesController < API::BaseController
   def index
     render json: ParcelSale.order(:parcel_id, :sales_date).page(params[:page]).per(params[:per_page])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     root 'root#index'
     resources :parcels, only: [:index, :show]
-    resources :parcel_sales, only: [:index, :show]
+    resources :sales, only: [:index, :show]
   end
 
   root 'housing#index'
