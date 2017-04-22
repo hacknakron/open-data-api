fb568f1c2abc04038cb8acbe1a12acc87ac10d58
Nick Kinney <nkinney@virtualhold.com>
2017-04-22 13:06:52 -0400

    Added a Parcel Sales Route.

---
+++ app/controllers/api/parcel_sales_controller.rb
@@ -0,0 +1,9 @@
+class API::ParcelSalesController < API::BaseController
+  def index
+    render json: ParcelSale.order(:parcel_id, :sales_date).page(params[:page]).per(params[:per_page])
+  end
+
+  def show
+    render json: ParcelSale.where(parcel_id: params[:id]).order(:sales_date)
+  end
+end
