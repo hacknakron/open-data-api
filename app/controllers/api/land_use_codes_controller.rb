2fc29497dd66667d25ff0918a21c8d3902703c7c
Marko Iskander <markoiskander@gmail.com>
2017-04-22 17:36:15 -0400

    Add another graph and update titles.

--- app/controllers/api/land_use_codes_controller.rb
+++ app/controllers/api/land_use_codes_controller.rb
@@ -2,16 +2,7 @@ class API::LandUseCodesController < API::BaseController
   def classes
     render json: {
       data: [
-        { use_class: 'A', name: 'Agriculture' },
-        { use_class: 'M', name: 'Mineral Rights' },
-        { use_class: 'I', name: 'Industrial' },
-        { use_class: 'O', name: 'Oil and Gas' },
-        { use_class: 'C', name: 'Commercial' },
-        { use_class: 'R', name: 'Residential' },
-        { use_class: 'E', name: 'Exempt' },
-        { use_class: 'P', name: 'Public Utilities' },
-        { use_class: 'G', name: 'Oil and Gas interests' },
-        { use_class: 'U', name: 'Railroad' },
+       LandUseCode::CLASSES,
       ]
     }
   end
--- app/controllers/housing_controller.rb
+++ app/controllers/housing_controller.rb
@@ -5,6 +5,8 @@ class HousingController < ApplicationController
     @avg_sales_by_year = avg_sales_by_year
     @city_count = Parcel.where(:city_owned => true).count
     @other_count = Parcel.where(:city_owned => false).count
+
+    @use_class_count = use_class_count
   end
   
   
@@ -26,11 +28,24 @@ class HousingController < ApplicationController
 
   def outside_akron_owners_by_state_data
     sql = <<-SQL
-      SELECT owner_state, COUNT(id) AS owner_count
+      SELECT CONCAT('US-',owner_state), COUNT(id) AS owner_count
       FROM parcels
       WHERE owner_state != 'OH' AND owner_state != ''
       GROUP BY owner_state
     SQL
     ActiveRecord::Base.connection.execute(sql).map { |i| i.values }
+  end
+
+  def use_class_count
+    sql = <<-SQL
+    SELECT use_class, COUNT(Id)
+    FROM parcels
+    GROUP BY use_class
+    SQL
+    ActiveRecord::Base.connection.execute(sql).map do |i|
+      data = i.values
+      [LandUseCode::CLASSES_LOOKUP[data.first], data.second]
+
+    end
   end
 end
