e679076e1bcdacb263678df8aa5a8148adb7ed74
Nick Kinney <nkinney@virtualhold.com>
2017-04-22 16:16:18 -0400

    Added meta data for the number of records and the number of pages.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,2 +1,13 @@
 class API::BaseController < ActionController::API
+	protected
+	def create_index_data objects
+		objects = objects.page(params[:page]).per(params[:per_page])
+		{
+	      data: objects,
+	      meta: {
+	      	count: objects.total_count,
+	      	pages: objects.total_pages
+	      }
+	 	}
+	end
 end
--- app/controllers/api/land_use_codes_controller.rb
+++ app/controllers/api/land_use_codes_controller.rb
@@ -1,9 +1,11 @@
 class API::LandUseCodesController < API::BaseController
+
   def index
-    render json: LandUseCode.order(:id).where(filter_params).page(params[:page]).per(params[:per_page])
+    render json: create_index_data(LandUseCode.order(:id).where(filter_params))
   end
+
   def classes
-    render json: [{use_class:'A', name: 'Agriculture'},  
+    render json: {data: [{use_class:'A', name: 'Agriculture'},  
     {use_class:'M', name:'Mineral Rights'},
     {use_class:'I', name:'Industrial'},
     {use_class:'O', name: 'Oil and Gas'},
@@ -12,8 +14,9 @@ class API::LandUseCodesController < API::BaseController
     {use_class:'E', name:'Exempt'},
     {use_class:'P', name:'Public Utilities'},
     {use_class:'G', name:'Oil and Gas interests'},
-    {use_class:'U', name:'Railroad'}]
+    {use_class:'U', name:'Railroad'}]}
   end
+  
   def show
     render json: LandUseCode.find_by(value: params[:id])
   end
