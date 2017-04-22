00697125f788663d4af9fe6cb4accfd9030df14d
Jonathan Hostetler <jhostetler@wastebits.com>
2017-04-22 15:01:37 -0400

    feat(outside_akron_owners_by_state): add heapmap partial for heapmap of owners outside of akron by state

--- app/controllers/housing_controller.rb
+++ app/controllers/housing_controller.rb
@@ -2,6 +2,19 @@ class HousingController < ApplicationController
   
   def index
     @housing_data = 'Views/housing/index.html.erb data'
+    @outside_akron_owners_by_state = outside_akron_owners_by_state_data
+  end
+  
+  
+  private
+  def outside_akron_owners_by_state_data
+    sql = <<-SQL
+      SELECT owner_state, COUNT(id) AS owner_count
+      FROM parcels
+      WHERE owner_city != 'AKRON' AND owner_city != ''
+      GROUP BY owner_state, owner_city
+    SQL
+    ActiveRecord::Base.connection.execute(sql).map { |i| i.values }
   end
   
 end
