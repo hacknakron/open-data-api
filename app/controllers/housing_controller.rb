60fcd822cfad9dbc1ea418cde6450f737700f536
Marko Iskander <iskander@adobe.com>
2017-04-22 15:17:10 -0400

    added parcel count piechart

--- app/controllers/housing_controller.rb
+++ app/controllers/housing_controller.rb
@@ -3,6 +3,8 @@ class HousingController < ApplicationController
   def index
     @housing_data = 'Views/housing/index.html.erb data'
     @outside_akron_owners_by_state = outside_akron_owners_by_state_data
+    @city_count = Parcel.where(:city_owned => true).count
+    @other_count = Parcel.where(:city_owned => false).count
   end
   
   
