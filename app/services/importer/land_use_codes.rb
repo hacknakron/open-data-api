14fe1eccbfdc5e7b7aca98a85f854874edee2da5
kharsch <kharsch@brandeis.edu>
2017-04-22 13:10:28 -0400

    db migrations and import changes

--- app/services/importer/land_use_codes.rb
+++ app/services/importer/land_use_codes.rb
@@ -4,8 +4,9 @@ class Importer::LandUseCodes < Importer
     super
     data.each do |row|
       land_use_code = ::LandUseCode.find_or_initialize_by({object_id: row[:objectid]})
-      land_use_code.assign_attributes({object_id: convert_int(row[:objectid]), value: convert_int(row[:val]), label: convert_string(row[:msg])})
-
+      use_class=convert_string(row[:msg]).split[0]
+      land_use_code.assign_attributes({object_id: convert_int(row[:objectid]), value: convert_int(row[:val]),
+       label: convert_string(row[:msg]), use_class: use_class})
       land_use_code.save! if land_use_code.changed? || land_use_code.new_record?
     end
   end
