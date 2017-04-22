fbc90cd8a99d423930581cdbf54143ba20fe5607
Eric Browning <eric@coffeeandcode.com>
2017-04-22 10:37:15 -0400

    Allow view rendering in app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,24 @@
 class HomeController < ApplicationController
   def home
-    @response = {
-      data: {
-        organization: 'Hack N Akron',
-        project: {
-          name: 'Open Data API',
-          repository: 'https://github.com/hacknakron/open-data-api'
-        },
-        location: {
-          address1: '',
-          address2: '',
-          city: 'Akron',
-          country: 'United States',
-          state: 'OH',
-          zip_code: '44308'
-        }
-      },
-      errors: {}
-    }
+    # @response = {
+    #   data: {
+    #     organization: 'Hack N Akron',
+    #     project: {
+    #       name: 'Open Data API',
+    #       repository: 'https://github.com/hacknakron/open-data-api'
+    #     },
+    #     location: {
+    #       address1: '',
+    #       address2: '',
+    #       city: 'Akron',
+    #       country: 'United States',
+    #       state: 'OH',
+    #       zip_code: '44308'
+    #     }
+    #   },
+    #   errors: {}
+    # }
 
-    render json: @response
+    # render json: @response
   end
 end
